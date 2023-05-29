1fa341b0e9f22c135b5ce84e62769b06704a969c
warittinan <warittinan.khun@kmutt.ac.th>
2023-05-29 17:57:53 +0700

    11

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+const API_URL = `http://lvm64075.sit.kmutt.ac.th:8080/api/students`
+
+export default {
+  data: () => ({
+    date: null
+  }),
+
+  created() {
+    // fetch on init
+    this.fetchData()
+  },
+
+  methods: {
+    async fetchData() {
+      const url = `${API_URL}`
+      this.date = await (await fetch(url)).json()
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Student</h1>
+
+  <p>{{ date }}</p>
+  
+  </div>
+</template>
